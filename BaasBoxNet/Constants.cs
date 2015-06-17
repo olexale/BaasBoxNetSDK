90c2695165228153b2b0c4fd6190c2c4122acd53
Olexandr Leuschenko <ole@ciklum.com>
2015-06-17 10:18:02 +0300

    Initial commit.

---
+++ BaasBoxNet/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace BaasBoxNet
+{
+    public static class Constants
+    {
+        public const string VersionName = "0.9.3-SNAPSHOT";
+        public const string ApiVersions = "0.9.3";
+        public const string MinApiVersion = "0.9.3";
+    }
+}
