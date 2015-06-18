939013c8154122cdaa583034a5872e1d7dd42f74
Olexandr Leuschenko <ole@ciklum.com>
2015-06-19 01:53:39 +0300

    Simple sample WP app added

---
+++ BBWPDemo/DesignTime/DesignLoginPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+using BBWPDemo.ViewModels;
+
+namespace BBWPDemo.DesignTime
+{
+    public class DesignLoginPageViewModel : ILoginPageViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public ICommand Login { get; private set; }
+        public ICommand Signup { get; private set; }
+    }
+}
---
+++ BBWPDemo/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BaasBoxNet;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace BBWPDemo.ViewModels
+{
+    internal class LoginPageViewModel : ViewModel, ILoginPageViewModel
+    {
+        private readonly IBaasBoxUserManagement _boxUserManagement;
+        private ICommand _login;
+        private string _password;
+        private ICommand _signup;
+        private string _username;
+
+        public LoginPageViewModel(IBaasBoxUserManagement boxUserManagement)
+        {
+            _boxUserManagement = boxUserManagement;
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set { SetProperty(ref _username, value); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public ICommand Login
+        {
+            get { return _login ?? (_login = new DelegateCommand(async () => await DoLogin())); }
+        }
+
+        public ICommand Signup
+        {
+            get { return _signup ?? (_signup = new DelegateCommand(async () => await DoSignup())); }
+        }
+
+        private Task DoLogin()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task DoSignup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
