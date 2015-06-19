26455d01b733b0f530a76171e46b5ca4504c9087
Olexandr Leuschenko <ole@ciklum.com>
2015-06-19 13:54:32 +0300

    ResetPassword fixed

---
+++ BBWPDemo/ViewModels/UserManagementPageViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Popups;
+using BaasBoxNet;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+using Microsoft.Practices.Prism.Mvvm.Interfaces;
+
+namespace BBWPDemo.ViewModels
+{
+    internal class UserManagementPageViewModel : ViewModel, IUserManagementPageViewModel
+    {
+        private readonly BaasBox _box;
+        private readonly INavigationService _navigationService;
+        private readonly IBaasBoxUserManagement _userManagement;
+        private ICommand _changePassword;
+        private ICommand _logout;
+        private string _newPassword;
+        private string _oldPassword;
+        private ICommand _resetPassword;
+
+        public UserManagementPageViewModel(INavigationService navigationService, BaasBox box,
+            IBaasBoxUserManagement userManagement)
+        {
+            _navigationService = navigationService;
+            _box = box;
+            _userManagement = userManagement;
+        }
+
+        public ICommand Logout
+        {
+            get { return _logout ?? (_logout = new DelegateCommand(async () => await DoLogout())); }
+        }
+
+        public ICommand ResetPassword
+        {
+            get
+            {
+                return _resetPassword ?? (_resetPassword = new DelegateCommand(async () => await DoResetPassword()));
+            }
+        }
+
+        public ICommand ChangePassword
+        {
+            get
+            {
+                return _changePassword ?? (_changePassword = new DelegateCommand(async () => await DoChangePassword()));
+            }
+        }
+
+        public string OldPassword
+        {
+            get { return _oldPassword; }
+            set { SetProperty(ref _oldPassword, value); }
+        }
+
+        public string NewPassword
+        {
+            get { return _newPassword; }
+            set { SetProperty(ref _newPassword, value); }
+        }
+
+        private async Task DoLogout()
+        {
+            try
+            {
+                await _userManagement.LogoutAsync();
+                _navigationService.Navigate(Experiences.Login.ToString(), null);
+            }
+            catch (Exception e)
+            {
+                new MessageDialog(e.Message).ShowAsync();
+            }
+        }
+
+        private async Task DoResetPassword()
+        {
+            try
+            {
+                await _userManagement.ResetPasswordAsync(_box.User.Name);
+                await new MessageDialog("done").ShowAsync();
+            }
+            catch (Exception e)
+            {
+                new MessageDialog(e.Message).ShowAsync();
+            }
+        }
+
+        private async Task DoChangePassword()
+        {
+            try
+            {
+                await _userManagement.ChangePasswordAsync(OldPassword, NewPassword);
+                await new MessageDialog("done").ShowAsync();
+            }
+            catch (Exception e)
+            {
+                new MessageDialog(e.Message).ShowAsync();
+            }
+        }
+    }
+}
--- BaasBoxNet/BaasBoxUserManagement.cs
+++ BaasBoxNet/BaasBoxUserManagement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -57,9 +58,16 @@ namespace BaasBoxNet
             };
             var jsonData = JsonConvert.SerializeObject(requestBody);
             var data = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var user = await _box.RestService.PostAsync<BaasUser>("user", data, cancellationToken);
-            _box.User = user;
-            return user;
+            var user = await _box.RestService.PostAsync<LoginResponse>("user", data, cancellationToken);
+            _box.User = new BaasUser
+            {
+                Name = user.User.Name,
+                Roles = user.User.Roles,
+                Session = user.Session,
+                SignupDate = user.SignupDate,
+                Status = user.User.Status
+            };
+            return _box.User;
         }
 
         public async Task<BaasUser> LoginAsync(string username, string password, CancellationToken cancellationToken)
@@ -71,9 +79,16 @@ namespace BaasBoxNet
                 {"appcode", _box.Config.AppCode}
             };
             var request = new FormUrlEncodedContent(requestBody);
-            var user = await _box.RestService.PostAsync<BaasUser>("login", request, cancellationToken);
-            _box.User = user;
-            return user;
+            var user = await _box.RestService.PostAsync<LoginResponse>("login", request, cancellationToken);
+            _box.User = new BaasUser
+            {
+                Name = user.User.Name,
+                Roles = user.User.Roles,
+                Session = user.Session,
+                SignupDate = user.SignupDate,
+                Status = user.User.Status
+            };
+            return _box.User;
         }
 
         public async Task LogoutAsync(CancellationToken cancellationToken)
@@ -98,6 +113,18 @@ namespace BaasBoxNet
         {
             var requestUrl = string.Format("user/{0}/password/reset", username);
             return _box.RestService.GetAsync<object>(requestUrl, cancellationToken);
+        }
+
+        private class LoginResponse
+        {
+            [JsonProperty("signUpDate")]
+            public DateTime SignupDate { get; set; }
+
+            [JsonProperty("X-BB-SESSION")]
+            public string Session { get; set; }
+
+            [JsonProperty("user")]
+            public BaasUser User { get; set; }
         }
     }
 }
