e9ae13d7a1ac885e341b1dff1ccb809d7a41d846
Olexandr Leuschenko <ole@ciklum.com>
2015-06-20 23:03:12 +0300

    Collections interface created

---
+++ BaasBoxNet/BaasBoxCollections.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BaasBoxNet
+{
+    public class BaasBoxCollections : IBaasBoxCollections
+    {
+        public Task CreateCollectionAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCollectionAsync(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BaasBoxNet/IBaasBoxCollections.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace BaasBoxNet
+{
+    public interface IBaasBoxCollections
+    {
+        Task CreateCollectionAsync(string name);
+        Task DeleteCollectionAsync(string name);
+    }
+}
